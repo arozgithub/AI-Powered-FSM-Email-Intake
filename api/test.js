bcaf91c66db3861055f05b9300b6ea2fd00eea8d
arozgithub <arozimran18@gmail.com>
2026-01-30 19:14:21 +0500

    Add test API endpoint

---
+++ api/test.js
@@ -0,0 +1,7 @@
+module.exports = async function handler(req, res) {
+  res.status(200).json({ 
+    message: 'API endpoint is working!',
+    timestamp: new Date().toISOString(),
+    method: req.method 
+  });
+}
